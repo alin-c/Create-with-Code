9efcc5e092e2ada4bafcb7db7284a4de3c8e7e60
Alin <alin.clincea@outlook.com>
2021-08-25 16:21:44 +0300

    * tutorial advance

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -11,5 +11,6 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		// Move the vehicle forward
+		transform.Translate(Vector3.forward * Time.deltaTime * 20);
 	}
 }
