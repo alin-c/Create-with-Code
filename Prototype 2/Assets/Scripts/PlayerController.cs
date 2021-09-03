c6d0896a712a1dbab1eeff491363ae8ea3f82bc0
Alin <alin.clincea@outlook.com>
2021-09-03 10:56:08 +0300

    * new gitignore file

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float horizontalInput;
-
+	public float speed = 10;
 
 	void Update() {
 		horizontalInput = Input.GetAxis("Horizontal");
+		transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
 	}
 }
