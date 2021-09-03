5448c8aefb82e754bf1793cf9287e6022b0ca6c2
Alin <alin.clincea@outlook.com>
2021-09-03 14:05:00 +0300

    * changed speed

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float horizontalInput;
-	public float speed = 10;
+	public float speed = 30;
 
 	void Update() {
 		horizontalInput = Input.GetAxis("Horizontal");
